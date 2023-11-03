697e78536fd075c2846fc9dc8947dbde7a75670e
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-11-03 12:49:06 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+const dt = luxon.DateTime;
 import { contactsList } from "./data.js";
 const { createApp } = Vue;
 
@@ -6,12 +6,41 @@ createApp({
     data(){
         return{
             contacts: contactsList,
+            activeContactIndex: 0,
+            searchText: '',
+            message: ''
         }
     },
     methods :{
+        selectContact(id){
+            const index = this.contacts.findIndex((contact)=> contact.id === id);
+            if(index !== -1){
+                this.activeContactIndex = index;
+            }
+        },
+        // getLastMessage(id){
+        //     const contact = this.contacts.find((contact)=> contact.id === id)
+        //     const len = contact.messages.lenght;
+        //     if(len > 0){
+        //         return contact.messages[len - 1].message; 
+        //     }else {
+        //         return 'Non ci sono messaggi';
+        //     }
+        // },
 
+        // getLastAccess(id){
+        //     const contact = this.contacts.find((contact)=> contact.id === id)
+        //     const len = contact.messages.lenght;
+        //     if(len > 0){
+        //         return contact.messages[len - 1].data; 
+        //     }else {
+        //         return 'Unknow';
+        //     }
+        // }
     },
     computed : {
-
+        // activeContact(){
+        //     return this.contacts(this.activeContactIndex);
+        // }
     }
 }).mount('#app')
